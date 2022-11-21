f1dcd593fa01f3138602a77dabe5f7e921b50cd4
endia <endia@endia.me>
2022-11-21 09:50:02 +0900

    설정버튼 CSS 수정,  README 수정

--- src/blog/main/component/Settings.js
+++ src/blog/main/component/Settings.js
@@ -1,36 +1,46 @@
-import { SettingFilled, UserOutlined } from '@ant-design/icons';
-import { Button, Dropdown, Menu, Space } from 'antd';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { SettingFilled, UserOutlined } from "@ant-design/icons";
+import { Button, Dropdown, Menu, Space } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
 
 /**
- * 
+ *
  * @param {object} param
- * @param {() => void} param.logout 
+ * @param {() => void} param.logout
  */
 export default function Settings({ logout }) {
-    return (
-        <>
-            <Space>
-                <Button type="ghost" shape='round' size='large'>
-                    <Link to={'/blog/write'}>글 작성하기</Link>
-                </Button>
-                <Dropdown
-                    overlayClassName="setting-dropbox"
-                    overlay={
-                        <Menu>
-                            <Menu.Item onClick={logout}>임시글</Menu.Item>
-                            <Menu.Item onClick={logout}>관심글</Menu.Item>
-                            <Menu.Item onClick={logout}>설정</Menu.Item>
-                            <Menu.Item onClick={logout}>로그아웃</Menu.Item>
-                        </Menu>
-                    }
-                    trigger={['click']}
-                    placement="bottomRight"
-                >
-                    <Button type='ghost' shape='circle' size='large' icon={<UserOutlined />} />
-                </Dropdown>
-            </Space>
-        </>
-    );
+  return (
+    <>
+      <Space>
+        <Button
+          className="write-button"
+          type="ghost"
+          shape="round"
+          size="large"
+        >
+          <Link to={"/blog/write"}>글 작성하기</Link>
+        </Button>
+        <Dropdown
+          overlayClassName="setting-dropbox"
+          overlay={
+            <Menu>
+              <Menu.Item onClick={logout}>임시글</Menu.Item>
+              <Menu.Item onClick={logout}>관심글</Menu.Item>
+              <Menu.Item onClick={logout}>설정</Menu.Item>
+              <Menu.Item onClick={logout}>로그아웃</Menu.Item>
+            </Menu>
+          }
+          trigger={["click"]}
+          placement="bottomRight"
+        >
+          <Button
+            type="ghost"
+            shape="circle"
+            size="large"
+            icon={<UserOutlined />}
+          />
+        </Dropdown>
+      </Space>
+    </>
+  );
 }
