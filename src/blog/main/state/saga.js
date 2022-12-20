f0f818f5727d2a038a7a064f54109435e9cc5b30
endia <endia@endia.me>
2022-12-20 22:56:09 +0900

    불필요한 로그 제거

--- src/blog/main/state/saga.js
+++ src/blog/main/state/saga.js
@@ -5,7 +5,6 @@ import { makeFetchSaga } from "../../../common/util/fetch";
 import { actions as common } from "../../../common/state";
 
 function* fetchAllPost(action, page) {
-  console.log(page, Math.floor(action.totalCount / 8))
   if (page <= Math.floor(action.totalCount / 8)) {
     const { isSuccess, data } = yield call(callApi, {
       url: "/post",
