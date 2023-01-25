5a6f0c931f06f32e9409052e807dc8a4fec832ff
endia <endia@endia.me>
2023-01-25 16:50:05 +0900

    게시글 앵커 순서오류 수정, 태그이름 노출 오류 수정

--- src/blog/post/components/Navigation.js
+++ src/blog/post/components/Navigation.js
@@ -24,64 +24,63 @@ export default function Navigation({ postContent }) {
   };
 
   useEffect(() => {
-    const regExTag = /(<h[1-5](.*?)>)(.*?)(<\/h[1-5]>)/gm;        // htag 매칭 정규식
-    const regExLevel = /(?<=level)([1-5])/g;                      // class 매칭 정규식
-    const regExTitle = /(?<=<h[1-5](.*?)>)(.*?)(?=<\/h[1-5]>)/g;  // title 매칭 정규식
-    const regExId = /(?<=id=")(.*?)(?=")/g;                       // id 매칭 정규식
-    const regExSpan = /(?<=<span(.*?)>)(.*?)(?=<\/span>)/g;       // span 매칭 정규식
-    const regExTstSpan = /(<span(.*?)>)(.*?)(<\/span>)/g;         // span 검증 정규식
+    const regExHTag = /(<h[1-5](.*?)>)(.*?)(<\/h[1-5]>)/gm;       // htag 매칭 정규식
     const array = [];                                             // 태그정보 최상위 배열
     let contents = postContent;
-    const htags = contents?.match(regExTag);
+    const htags = contents?.match(regExHTag);
     let prevLevel = 0;
     let prevAnchor = null;
-    let continueFlag = false; // 제목이 <br>인 경우 제어플레그
     /* 
       h1 ~ h5 태그를 컨텐츠에서 매칭해해서 계층화해서 랜더링한다.
       <br>이나 <span> 태그는 제거하는 정제작업을 부가적으로 수행한다.
     */
     htags?.map(tag => {
+      const regExLevel = /(?<=level)([1-5])/g;                      // class 매칭 정규식
+      const regExTitle = /(?<=<h[1-5](.*?)>)(.*?)(?=<\/h[1-5]>)/g;  // title 매칭 정규식
+      const regExId = /(?<=id=")(.*?)(?=")/g;                       // id 매칭 정규식
+      const regExTag = /(?<=<(.*?)>)(.*?)(?=<\/.*>)/g;              // 태그 매칭 정규식
+      const regExTstTag = /(<(.*?)>)(.*?)(<\/.*>)/gm;               // 태그 검증 정규식
+
       const level = tag?.match(regExLevel)?.[0];
-      let title = tag?.match(regExTitle)?.[0];
+      let title = tag?.match(regExTitle)?.[0].trim();
       const href = '#' + tag?.match(regExId);
 
-      /* 제목이 span으로 래핑되있을경우 재매핑 */
-      if (regExTstSpan.test(title)) {
-        title = title?.match(regExSpan)?.[0];
+      /* 제목이 태그로 래핑되있을경우 재매핑 */
+      if (regExTstTag.test(title)) {
+        title = title?.match(regExTag)?.[0];
       }
 
       /* 제목이 <br>이면 추가하지 않음 */
       if (title != '<br>') {
         if (!prevLevel) {
-          prevAnchor = { title, href, child: [], parent: null };
+          prevAnchor = { title, href, level, child: [], parent: null };
           array.push(prevAnchor);
         } else {
           /* 이전 앵커와 같은 레벨인 경우 */
           if (prevLevel === level) {
             if (prevAnchor?.parent) {
               const parent = prevAnchor?.parent;
-              prevAnchor = { title, href, child: [], parent };
+              prevAnchor = { title, href, level, child: [], parent };
               parent?.child?.push(prevAnchor);
             } else {
-              prevAnchor = { title, href, child: [], parent: null };
+              prevAnchor = { title, href, level, child: [], parent: null };
               array.push(prevAnchor);
             }
           }
           /* 이전 앵커보다 큰 레벨인 경우 */
           if (prevLevel < level) {
             const parent = prevAnchor;
-            prevAnchor = { title, href, child: [], parent };
+            prevAnchor = { title, href, level, child: [], parent };
             parent.child.push(prevAnchor);
           }
           /* 이전 앵커보다 작은 레벨인 경우 */
           if (prevLevel > level) {
             const parent = parentFind(prevAnchor, level);
-            console.log(parent);
             if (parent === -1) {
-              prevAnchor = { title, href, child: [], parent: null };
+              prevAnchor = { title, href, level, child: [], parent: null };
               array.push(prevAnchor);
             } else {
-              prevAnchor = { title, href, child: [], parent };
+              prevAnchor = { title, href, level, child: [], parent };
               parent.child.push(prevAnchor);
             }
           }
@@ -89,6 +88,7 @@ export default function Navigation({ postContent }) {
         prevLevel = level;
       }
     });
+    console.log(array);
     setIndexList(array);
   }, [postContent]);
   return (
@@ -103,7 +103,7 @@ export default function Navigation({ postContent }) {
               className="post-content-navigation"
               style={{ width: 275, borderRadius: 12 }}
             >
-              <Anchor targetOffset={targetOffset}>
+              <Anchor targetOffset={targetOffset} style={{ maxHeight: '70vh' }}>
                 {indexList?.map((item) => {
                   return <AnchorLink title={item.title} href={item.href} key={item.title} child={item.child} />
                 })}
